55f75b1bb90f74329ed65f09494437573c403f7d
PaceGG <114814535+PaceGG@users.noreply.github.com>
2023-10-25 21:01:40 +0500

    Create www.sh

---
+++ www.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+mkdir public_html
+# echo "<html><body>" > public_html/index.html
+echo "<h1>Guess a number game</h1>" > public_html/index.html
+# echo "<br><a href=\"1.html\">Start</a></body></html>" >> public_html/index.html
+
+numm=$((RANDOM % 100 + 1))
+for ((i=1; i<=100; i++))
+do
+        echo "<a href=\"$i.html\">$i</a>" >> public_html/index.html
+
+        if [ $i -eq $numm ]
+                then
+                        echo "<h1>YOU WIN</h1>" >> public_html/$i.html
+                else
+                        if [ $i -gt $numm ]
+                        then
+                                echo "<h1>number is less</h1>" >> public_html/$i.html
+                        else
+                                echo "<h1>number is greater</h1>" >> public_html/$i.html
+                fi
+        fi
+
+        echo "<br>" >> public_html/$i.html
+
+        for ((j=1; j<=100; j++))
+        do
+                echo "<a href=\"$j.html\">$j</a>" >> public_html/$i.html
+        done
+
+        echo "<br><br><a href=\"index.html\">home</a>" >> public_html/$i.html
+done
